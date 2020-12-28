a7ee202bc4de4b2ef7b704c247aed7c96a5fae21
octon21 <72507594+octon21@users.noreply.github.com>
2020-12-28 14:31:26 +0900

    Update func.c

--- func.c
+++ func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 typedef struct
 {
@@ -8,99 +9,57 @@ typedef struct
   double temp; // temperature (centigrade)
 } Sample;
 
-int f_dimension(Sample *data){
-  int n = 0;
-  while (data[n].loc != NULL){
-    n++;
-  }
-  return n ;
-}
-
-
 double f_value(const double x[], const Sample sample[], const int n)
 {
   double sum = 0;
-  for (int i = 0; i < n; i++){  // nはデータの数．x[0]がa, x[1]がb
-    sum += (sample[i].temp - x[0] * sample[i].alt - x[1]) * (sample[i].temp - x[0] * sample[i].alt - x[1]);
+  for (int i = 0; i < n; i++){//データ数n
+    sum +=(sample[i].temp -x[0] * sample[i].alt - x[1]) * (sample[i].temp - x[0] * sample[i].alt - x[1]);
   }
   return sum;
 }
 
 
-void f_gradient(const double x[], double g[], const Sample sample[], const int n)
+void f_gradient(const double x[], double g[], const Sample data[], const int n)
 {
   double sum0 = 0;
   double sum1 = 0;
   for (int i = 0; i < n; i++){
-    sum0 += (-2) * sample[i].alt * (sample[i].temp - x[0] * sample[i].alt - x[1]);
+    sum0 += (-2) *data[i].alt * (data[i].temp - x[0] * data[i].alt - x[1]);
   }
   for (int i = 0; i < n; i++){
-    sum1 += (-2) * (sample[i].temp - x[0] * sample[i].alt - x[1]);
+    sum1 += (-2) * (data[i].temp - x[0] *data[i].alt - x[1]);
   }
-  // 代入
   g[0] = sum0;
   g[1] = sum1;
 }
 
-
-/*int optimize(const double alpha, const int dim, double x[], 
-             void (*calc_grad)(const double [], double []))
-{
-  // 勾配ベクトルを記録する領域を確保
-  double *g = malloc(dim * sizeof(double));
-
-  int iter = 0;
-  while (++iter < 10000) {
-
-    // 引数で渡された関数を使って勾配ベクトルを計算
-    (*calc_grad)(x, g);
-
-    // 勾配ベクトルのノルムを評価
-    const double norm = calc_norm(dim, g);
-    printf("%3d norm = %7.4f", iter, norm);
-    for (int i = 0; i < dim; i++) {
-      printf(", x[%d] = %7.4f", i, x[i]);
-    }
-    printf("\n");
-
-    if (norm < 0.01) break;
-
-    // 最急降下法による更新
-    for (int i = 0; i < dim; i++) {
-      x[i] -= alpha * g[i];
-    }
-  }
-
-  free(g);
-
-  return iter;
-}*/
-
-int optimize(const double alpha, const int dim, double x[],Sample sample[],int n, void (*calc_grad)(const double x[], double g[], const Sample sample[], const int n),
-double (*calc_value)(const double x[], const Sample sample[], const int n))
+int optimize(const double alpha, const int dim, double x[],Sample data[], void (*calc_grad)(const double x[], double g[], const Sample data[], const int dim),
+double (*calc_value)(const double x[], const Sample data[], const int dim))
 {
     // 勾配ベクトル（今回は二次元ベクトル）を確保
     double *g = malloc(dim * sizeof(double));
     int iter = 0;
-    while(++iter < 100) {
-        (*calc_grad)(x, g, sample, n);
+    while(++iter < 10000) {
+        (*calc_grad)(x, g, data, dim);
         // 平均二乗誤差
-        const double err = (*calc_value)(x, sample, n) / n;
+        const double err = (*calc_value)(x, data, dim) / dim;
         // 表示
         printf("%3d mean_squared_error = %7.4f", iter, err);
-        for (int i = 0; i < dim; i++) {
+        for (int i = 0; i < 2; i++) {
             printf(", x[%d] = %7.4f", i, x[i]);
         }
-        for (int i = 0; i < dim; i++) {
+        for (int i = 0; i <  2; i++) {
             printf(", g[%d] = %7.4f", i, g[i]);
         }
-        printf(",  f(x) = %f\n", (*calc_value)(x, sample, n));
+        printf(",  f(x) = %f\n", (*calc_value)(x, data, dim));
         // 終了条件
-        if (err < 0.0001) {
+
+        if (err < 0.01) {          
             break;
         }
+
         // 最急降下法
-        for (int i = 0; i < dim; i++){
+        for (int i = 0; i < 2; i++){
             x[i] -= alpha * g[i];
         }
     }
