8e41d7184032c7c98185907ab992df35e638b831
octon21 <72507594+octon21@users.noreply.github.com>
2020-12-28 13:04:03 +0900

    Update mtfuji.c

--- mtfuji.c
+++ mtfuji.c
@@ -2,6 +2,14 @@
 #include<stdlib.h>
 #include "func.h"
 
+typedef struct
+{
+  char *loc;   // location name
+  double alt;  // altitude (km)
+  double temp; // temperature (centigrade)
+} data;
+
+
 int main(){
   int num=14;
   //Sample *data=(Sample)malloc(sizeof(Sample));
@@ -17,16 +25,24 @@ int main(){
   char  Loc[10];
   double Alt;
   double Temp;
+  char buf[100];
+  char str[100];
+  
   for(int i=0; i<num;i++){
-    fscanf(fp,"%[^,],%lf,%lf",data[i].loc, &data[i].alt, &data[i].temp);
-  //fscanf(fp,"%s %f %f",data[i].loc, &data[i].alt, &data[i].temp);
+    fscanf(fp,"%[^,],%lf,%lf",str, &data[i].alt, &data[i].temp);
+    data[i].loc=str;    
+    //printf("%s,%lf,%lf\n",data[i].loc,data[i].alt,data[i].temp);
   }
+
   //sort(Sample)引数a[]
-  sort(data);
-
-
-
-  estimate(data);//最小2zyouhou
+  //sort(data);
+   const int dim = f_dimension();
+    double *x = malloc(dim * sizeof(double));
+    // 適当な値でa, bを初期化
+    x[0] = 10.0;
+    x[1] = 10.0;
+    double alpha=0.1;
+    printf("alpha = %f\n", alpha);
+    // 最適化する関数を動かす
+    const int iter = optimize(alpha, dim, x, data, 2,f_gradient, f_value);
 }
-
-
