3021e2f23ef34d8f549aca2293c079427f8899a8
octon21 <72507594+octon21@users.noreply.github.com>
2020-12-28 23:26:09 +0900

    Update func.c

--- func.c
+++ func.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include<stdlib.h>
-#include<math.h>
 
 typedef struct
 {
@@ -9,11 +8,11 @@ typedef struct
   double temp; // temperature (centigrade)
 } Sample;
 
-double f_value(const double x[], const Sample sample[], const int n)
+double f_value(const double x[], const Sample data[], const int n)
 {
   double sum = 0;
   for (int i = 0; i < n; i++){//データ数n
-    sum +=(sample[i].temp -x[0] * sample[i].alt - x[1]) * (sample[i].temp - x[0] * sample[i].alt - x[1]);
+    sum +=(data[i].temp -x[0] * data[i].alt - x[1]) * (data[i].temp - x[0] * data[i].alt - x[1]);
   }
   return sum;
 }
@@ -36,33 +35,21 @@ void f_gradient(const double x[], double g[], const Sample data[], const int n)
 int optimize(const double alpha, const int dim, double x[],Sample data[], void (*calc_grad)(const double x[], double g[], const Sample data[], const int dim),
 double (*calc_value)(const double x[], const Sample data[], const int dim))
 {
-    // 勾配ベクトル（今回は二次元ベクトル）を確保
-    double *g = malloc(dim * sizeof(double));
-    int iter = 0;
-    while(++iter < 10000) {
-        (*calc_grad)(x, g, data, dim);
-        // 平均二乗誤差
-        const double err = (*calc_value)(x, data, dim) / dim;
-        // 表示
-        printf("%3d mean_squared_error = %7.4f", iter, err);
-        for (int i = 0; i < 2; i++) {
-            printf(", x[%d] = %7.4f", i, x[i]);
-        }
-        for (int i = 0; i <  2; i++) {
-            printf(", g[%d] = %7.4f", i, g[i]);
-        }
-        printf(",  f(x) = %f\n", (*calc_value)(x, data, dim));
-        // 終了条件
+  // 勾配ベクトル（今回は二次元ベクトル）を確保
+  double *g = malloc(dim * sizeof(double));
+  int iter = 0;
+  while(++iter < 10000) {
+    (*calc_grad)(x, g, data, dim);
+    // 平均二乗誤差
+    const double err = (*calc_value)(x, data, dim) / dim;
 
-        if (err < 0.01) {          
-            break;
-        }
-
-        // 最急降下法
-        for (int i = 0; i < 2; i++){
-            x[i] -= alpha * g[i];
-        }
+    // 終了条件
+    if (err < 0.01)break;
+    // 最急降下法
+    for (int i = 0; i < 2; i++){
+      x[i] -= alpha * g[i];
     }
-    free(g);
-    return iter;
+  }
+  free(g);
+  return iter;
 }
